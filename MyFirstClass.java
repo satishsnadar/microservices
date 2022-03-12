5df0dc7cea80b2065ba1637d9f7b84701dc64148
satishsnadar <101448588+satishsnadar@users.noreply.github.com>
2022-03-12 01:03:09 -0800

    MyFirstClass.java
    MyFirstClass.java

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+package com.satishsnadar.microservices.util;
+import java.io.*;
+import java.util.*;
+
+
+public class MyFirstClass {
+
+
+public static void main(String[] args){
+System.out.println(" My First Class");s
+}//main
+
+
+}//class
